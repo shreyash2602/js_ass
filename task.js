57625700c1a67ee5f2ffb8c0d3f370e660a5dd3f
shreyash2602 <82608490+shreyash2602@users.noreply.github.com>
2021-11-27 22:40:23 +0530

    Create task.js

---
+++ task.js
@@ -0,0 +1,21 @@
+var person1 = {
+  FirstName: "Anupriya",
+  LastName: "Jha",
+  OfficeAdress: "Darbhanga, Bihar",
+};
+
+var person2 = {
+  FirstName: "Shrutika",
+  LastName: "Umare",
+  OfficeAdress: "Near G.N college, Ballarpur, Maharashtra",
+};
+
+var person3 = {
+  FirstName: "Shraddha",
+  LastName: "Kapoor",
+  OfficeAdress: "7th Floor, Palm Beach, Mumbai, Maharashtra",
+};
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
